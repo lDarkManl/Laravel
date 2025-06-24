402ea07f0c80c5d74cd24b8dd4b9b2f9c87bffc9
dmitriy.gusev <d_gusev_04@mail.ru>
2025-06-24 09:38:06 +0700

    config

--- www/config.php
+++ www/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'mydb');
-define('DB_PASS', 'mydb');
-define('DB_NAME', 'mydb');
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
